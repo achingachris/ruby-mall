438bfbf3e1e316e9295f0adcaeacc6f082e1811b
ChrisAchinga <achinga.chris@gmail.com>
2020-10-18 21:32:56 +0300

    create solution files

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 Zeraki Technical Assesment
 
+Links to Solution:
+
+[task 1](task1.js)
+
+[task 2](task2.js)
+
+[task 3](db/database.sql)
+
 ![image](screenshot.png)
---
+++ task1.js
@@ -0,0 +1,3 @@
+// Given an array K with N integers from 1 to N+1 such that the array has exactly one integer missing, write
+// a Java function that returns the missing integer.
+// e.g. given K = [3,5,4,1], the function should return 2
